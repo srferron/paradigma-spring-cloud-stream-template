4c680d7233be815cb6c541c6a5d4881a45358d88
srferron <srferron@gmail.com>
2023-07-26 07:51:16 +0200

    Update $$everySchema$$.java

--- template/src/main/java/$$everySchema$$.java
+++ template/src/main/java/$$everySchema$$.java
@@ -5,4 +5,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 {% endif %}
 {% from "partials/java-class" import javaClass -%}
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
 {{ javaClass(schemaName, schema, schema.properties(), 0, false) }}
